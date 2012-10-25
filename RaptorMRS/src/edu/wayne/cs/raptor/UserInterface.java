20e3fab3a21b6f72b3ca89b207639d2021ccd7b0
scosgrove <sarahmcosgrove@gmail.com>
2012-10-24 20:59:10 -0700

    added JavaDoc type comments

--- RaptorMRS/src/edu/wayne/cs/raptor/UserInterface.java
+++ RaptorMRS/src/edu/wayne/cs/raptor/UserInterface.java
@@ -1,30 +1,96 @@
+/**
+ *  @author Sarah Cosgrove
+ *  @version 1.0
+ *  @since 2012-10-23
+ */
+
+
+
 package edu.wayne.cs.raptor;
 
 public class UserInterface {
-	
+	  
+	 /**
+	  * Name of user
+	  */
 	  private String username; 
+	  
+	  /**
+	   * Password phrase
+	   */
 	  private String password;
+	  
+	  /**
+	   * Result of login attempt
+	   */
 	  private String loginAttemptResult;
 	  
+	  /**
+	   * Retrieves the user name
+	   * 
+	   * This function returns the user's name
+	   * 
+	   */
 	  public String getUsername() {
 	    return username;
 	  }
+	  
+	  /**
+	   * Sets the user name
+	   * 
+	   * This function sets the user's name to what is specified in the parameter.
+	   * 
+	   */
 	  public void setUsername(String username) {
 	    this.username = username;
 	  }
+	  
+	  /**
+	   * Retrieves the password
+	   * 
+	   * This function returns the user's password
+	   * 
+	   */
 	  public String getPassword() {
 	    return password;
 	  }
-	  
+	   
+	  /**
+	   * Sets the password
+	   * 
+	   * This function sets the user password
+	   * 
+	   */
 	  public void setPassword(String password) {
 		    this.password = password;
 	  }
+	  
+	  /**
+	   * Retrieves the login result
+	   * 
+	   * This function returns a String message describing the result of the login attempt.
+	   * 
+	   */
 	  public String getLoginAttemptResult() {
 		    return loginAttemptResult;
 		  }
+	  
+	  /**
+	   * Sets the login result string
+	   * 
+	   * This method sets the login result string
+	   * 
+	   */
 	  public void setloginAttemptResult(String loginAttemptResult) {
 		    this.loginAttemptResult = loginAttemptResult;
 		  }
+	  
+	  /**
+	   * Checks password
+	   * 
+	   * This method checks the password to see if it matches the string "raptor" and sets the login attempt result string based on the comparison.
+	   * 
+	   */
 	  public String checkPassword() {
 		  //search database for password
 		  if (this.password.equals(new String("raptor"))) {
