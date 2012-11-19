3171ffeac1d5cddb0d8ad4a83091ab4d0a8f88b1
Ramez Habib <ramezhabib@gmail.com>
2012-11-19 03:19:56 -0500

    added PatientService, UserService, constants for Permissions and Roles

---
+++ RaptorMRS/src/edu/wayne/cs/raptor/Permission.java
@@ -0,0 +1,22 @@
+package edu.wayne.cs.raptor;
+
+
+/** Permissions and their descriptions in the system
+ *   
+ * 
+ * @author Ramez
+ * */
+
+public class Permission {
+	
+	
+	public static final String MODIFY_PATIENTS = "Modify Patients";
+	
+	public static final String MODIFY_USERS = "Modify Users";
+	
+	public static final String DISPENSE_MED = "Dispense Medication";
+	
+	
+	
+
+}
