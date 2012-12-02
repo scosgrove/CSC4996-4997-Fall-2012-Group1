960e16f3ffb60fadcf49b9708716177659c9f4d5
Sarah Cosgrove <sarahmcosgrove@gmail.com>
2012-12-02 18:21:09 -0500

    adding database query functionality to blood sample report

--- RaptorMRS/src/edu/wayne/cs/raptor/ResearchReportsBean.java
+++ RaptorMRS/src/edu/wayne/cs/raptor/ResearchReportsBean.java
@@ -19,310 +19,341 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-
-
-
 public class ResearchReportsBean {
-	
+
 	PharmacyEncounter ec = new PharmacyEncounter();
-	
-	public static void generateBloodSampleReport(){
-		
-		
-	   
-		   String filename = null;
-		   JFileChooser jFileChooser = new JFileChooser();
-		   jFileChooser.setSelectedFile(new File(suggestFileName() + "-BloodSample.csv"));
-		   int returnValue = jFileChooser.showSaveDialog(null);
-		   if ( returnValue == jFileChooser.APPROVE_OPTION )
-		   {
-			   File selectedFile = jFileChooser.getSelectedFile();
-			   generateBloodSampleCsvFile(selectedFile);
-		   }
-	   }
-	   
-	   private static void generateBloodSampleCsvFile(File filehandle)
-	   {
-		   try
-		   {
-			   FileWriter writer = new FileWriter(filehandle);
 
-			   //Get Data from raptor
-			   
-			   Session userSession = HibernateUtil.getSessionFactory().openSession();
-				userSession.beginTransaction();
+	public static void generateBloodSampleReport() {
+
+		String filename = null;
+		JFileChooser jFileChooser = new JFileChooser();
+		jFileChooser.setSelectedFile(new File(suggestFileName()
+				+ "-BloodSample.csv"));
+		int returnValue = jFileChooser.showSaveDialog(null);
+		if (returnValue == jFileChooser.APPROVE_OPTION) {
+			File selectedFile = jFileChooser.getSelectedFile();
+			generateBloodSampleCsvFile(selectedFile);
+		}
+	}
+
+	private static void generateBloodSampleCsvFile(File filehandle) {
+		try {
+			FileWriter writer = new FileWriter(filehandle);
+
+			// Get Data from raptor
+
+			Session userSession = HibernateUtil.getSessionFactory()
+					.openSession();
+			userSession.beginTransaction();
+			List<Encounter> dbencounter = userSession.createQuery("from Encounter").list();
+			//List<Vitals> dbvitals = userSession.createQuery("from Vitals").list();
+			List<PharmacyEncounter> dbpharm = userSession.createQuery("from PharmacyEncounter").list();
+			userSession.getTransaction().commit();
+			userSession.close();
+
+			writer.append("Blood Sample Report");
+			writer.append('\n');
+			writer.append("Blood Sample ID");
+			writer.append(',');
+			writer.append("Condition");
+			writer.append(',');
+			writer.append("Condition 2");
+			writer.append(',');
+			writer.append("Condition 3");
+			writer.append(',');
+			writer.append("Condition 4");
+			writer.append(',');
+			writer.append("Condition 5");
+			writer.append(',');
+			writer.append("Medical Procedure(s)"); 
+			writer.append(',');
+			writer.append("Rx Prescribed1");
+			writer.append(',');
+			writer.append("Rx Prescribed2");
+			writer.append(',');
+			writer.append("Rx Prescribed3");
+			writer.append(',');
+			writer.append("Rx Prescribed4");
+			writer.append(',');
+			writer.append("Rx Prescribed5");
+			writer.append(',');
+			writer.append("RX Dispensed");
+			writer.append(',');
+			writer.append("RX Dispensed 2");
+			writer.append(',');
+			writer.append("RX Dispensed 3");
+			writer.append(',');
+			writer.append("RX Dispensed 4");
+			writer.append(',');
+			writer.append("RX Dispensed 5");
 			
-				
-				List<PharmacyEncounter> dbUsername = userSession.createQuery( "from PharmacyEncounter").list();
-				userSession.getTransaction().commit(); 
-				userSession.close();
-				
-				
-				writer.append("Blood Sample Report");
+			writer.append('\n');
+
+			for (int i = 0; i < dbencounter.size(); i++) {
+				//writer.append(String.valueOf(dbvitals.get(i).getBloodSampleID()));
+				writer.append(',');
+				writer.append(dbencounter.get(i).getCondition1());
+				writer.append(',');
+				writer.append(dbencounter.get(i).getCondition2());
+				writer.append(',');
+				writer.append(dbencounter.get(i).getCondition3());
+				writer.append(',');
+				writer.append(dbencounter.get(i).getCondition4());
+				writer.append(',');
+				writer.append(dbencounter.get(i).getCondition5());
+				writer.append(',');
+				writer.append(dbencounter.get(i).getMedicalProcedures());
+				writer.append(',');
+				//writer.append(dbencounter.get(i).getMedicationPrescribed1());
+				writer.append(',');
+				//writer.append(dbencounter.get(i).getMedicationPrescribed2());
+				writer.append(',');
+				//writer.append(dbencounter.get(i).getMedicationPrescribed3());
+				writer.append(',');
+				//writer.append(dbencounter.get(i).getMedicationPrescribed4());
+				writer.append(',');
+				//writer.append(dbencounter.get(i).getMedicationPrescribed5());
+				writer.append(',');
+				writer.append(dbpharm.get(i).getMedDispensed1());
+				writer.append(',');
+				writer.append(dbpharm.get(i).getMedDispensed2());
+				writer.append(',');
+				writer.append(dbpharm.get(i).getMedDispensed3());
+				writer.append(',');
+				writer.append(dbpharm.get(i).getMedDispensed4());
+				writer.append(',');
+				writer.append(dbpharm.get(i).getMedDispensed5());
+				writer.append(',');
 				writer.append('\n');
-				writer.append("SAMPLE NO.");
+				// generate whatever data you want
+
+			}
+			writer.flush();
+			writer.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void generateConditionReport() {
+		String filename = null;
+		JFileChooser jFileChooser = new JFileChooser();
+		jFileChooser.setSelectedFile(new File(suggestFileName()
+				+ "-ConditionReport.csv"));
+		int returnValue = jFileChooser.showSaveDialog(null);
+		if (returnValue == jFileChooser.APPROVE_OPTION) {
+			File selectedFile = jFileChooser.getSelectedFile();
+			generateConditionFile(selectedFile);
+		}
+	}
+
+	private static void generateConditionFile(File filehandle) {
+		try {
+			FileWriter writer = new FileWriter(filehandle);
+
+			// Get Data from raptor
+
+			Session userSession = HibernateUtil.getSessionFactory()
+					.openSession();
+			userSession.beginTransaction();
+
+			List<Encounter> dbencounter = userSession.createQuery(
+					"from Encounter").list();
+			List<PharmacyEncounter> dbpharm = userSession.createQuery(
+					"from PharmacyEncounter").list();
+			userSession.getTransaction().commit();
+			userSession.close();
+
+			writer.append("Conditon Report");
+			writer.append('\n');
+
+			writer.append("Sample#");
+			writer.append(',');
+			writer.append("Condition1");
+			writer.append(',');
+			writer.append("Condition2");
+			writer.append(',');
+			writer.append("Condition3");
+			writer.append(',');
+			writer.append("Condition4");
+			writer.append(',');
+			writer.append("Condition5");
+			writer.append(',');
+			writer.append("Rx Prescribed1");
+			writer.append(',');
+			writer.append("Rx Prescribed2");
+			writer.append(',');
+			writer.append("Rx Prescribed3");
+			writer.append(',');
+			writer.append("Rx Prescribed4");
+			writer.append(',');
+			writer.append("Rx Prescribed5");
+			writer.append(',');
+			/*
+			 * writer.append("Rx Given1"); writer.append('\n');
+			 * writer.append("Rx Given2"); writer.append(',');
+			 * writer.append("Rx Given3"); writer.append(',');
+			 * writer.append("Rx Given4"); writer.append(',');
+			 * writer.append("Rx Given5");
+			 */
+			writer.append('\n');
+
+			for (int i = 0; i < dbencounter.size(); i++) {
+				writer.append(String.valueOf(dbencounter.get(i)
+						.getEncounterID()));
 				writer.append(',');
-				writer.append("CONDITIONS");
+				writer.append(dbencounter.get(i).getCondition1());
 				writer.append(',');
-				writer.append("TREATMENT GIVEN");
+				writer.append(dbencounter.get(i).getCondition2());
 				writer.append(',');
-				writer.append("RX WRITTEN");
+				writer.append(dbencounter.get(i).getCondition3());
 				writer.append(',');
-				writer.append("RX GIVEN");
+				writer.append(dbencounter.get(i).getCondition4());
+				writer.append(',');
+				writer.append(dbencounter.get(i).getCondition5());
+				writer.append(',');
+				writer.append(dbpharm.get(i).getMedDispensed1());
+				writer.append(',');
+				writer.append(dbpharm.get(i).getMedDispensed2());
+				writer.append(',');
+				writer.append(dbpharm.get(i).getMedDispensed3());
+				writer.append(',');
+				writer.append(dbpharm.get(i).getMedDispensed4());
+				writer.append(',');
+				writer.append(dbpharm.get(i).getMedDispensed5());
 				writer.append('\n');
-				
-				for (int i = 0; i < dbUsername.size(); i++)
-				{
-					
-					writer.append(dbUsername.get(i).getMedDispensed1());
-					writer.append('\n');
-			   
-			   
-			   //generate whatever data you want
-		 
 			}
-				writer.flush();
-				writer.close();
-		   }
-			catch(IOException e)
-			{
-				e.printStackTrace();
-			}
-	   }
-	   
-	   public static void generateConditionReport()
-	   {
-		   String filename = null;
-		   JFileChooser jFileChooser = new JFileChooser();
-		   jFileChooser.setSelectedFile(new File(suggestFileName() + "-ConditionReport.csv"));
-		   int returnValue = jFileChooser.showSaveDialog(null);
-		   if ( returnValue == jFileChooser.APPROVE_OPTION )
-		   {
-			   File selectedFile = jFileChooser.getSelectedFile();
-			   generateConditionFile(selectedFile);
-		   }
-	   }
-	 
-	   private static void generateConditionFile(File filehandle)
-	   {
-		   try
-		   {
-			   FileWriter writer = new FileWriter(filehandle);
 
-			   //Get Data from raptor
-			   
-			   Session userSession = HibernateUtil.getSessionFactory().openSession();
-				userSession.beginTransaction();
-			
-				
-				List<Encounter> dbencounter = userSession.createQuery( "from Encounter").list();
-				List<PharmacyEncounter> dbpharm = userSession.createQuery( "from PharmacyEncounter").list();
-				userSession.getTransaction().commit(); 
-				userSession.close();
-			   
-				
-			   writer.append("Conditon Report");
-			   writer.append('\n');
-			   
-			   writer.append("Sample#");
-			   writer.append(',');
-			   writer.append("Condition1");
-			   writer.append(',');
-			   writer.append("Condition2");
-			   writer.append(',');
-			   writer.append("Condition3");
-			   writer.append(',');
-			   writer.append("Condition4");
-			   writer.append(',');
-			   writer.append("Condition5");
-			   writer.append(',');
-			   writer.append("Rx Prescribed1");
-			   writer.append(',');
-			   writer.append("Rx Prescribed2");
-			   writer.append(',');
-			   writer.append("Rx Prescribed3");
-			   writer.append(',');
-			   writer.append("Rx Prescribed4");
-			   writer.append(',');
-			   writer.append("Rx Prescribed5");
-			   writer.append(',');
-			 /*
-			   writer.append("Rx Given1");
-			   writer.append('\n');
-			   writer.append("Rx Given2");
-			   writer.append(',');
-			   writer.append("Rx Given3");
-			   writer.append(',');
-			   writer.append("Rx Given4");
-			   writer.append(',');
-			   writer.append("Rx Given5");
-			   */
-			   writer.append('\n');
-			   
-			   
-			   for (int i = 0; i < dbencounter.size(); i++)
-			   {
-				    writer.append(String.valueOf(dbencounter.get(i).getEncounterID()));
-				    writer.append(',');
-				    writer.append(dbencounter.get(i).getCondition1());
-				    writer.append(',');
-				    writer.append(dbencounter.get(i).getCondition2());
-				    writer.append(',');
-				    writer.append(dbencounter.get(i).getCondition3());
-				    writer.append(',');
-				    writer.append(dbencounter.get(i).getCondition4());
-				    writer.append(',');
-				    writer.append(dbencounter.get(i).getCondition5());
-				    writer.append(',');
-				    writer.append(dbpharm.get(i).getMedDispensed1());
-					writer.append(',');
-					writer.append(dbpharm.get(i).getMedDispensed2());
-					writer.append(',');
-					writer.append(dbpharm.get(i).getMedDispensed3());
-					writer.append(',');
-					writer.append(dbpharm.get(i).getMedDispensed4());
-					writer.append(',');
-					writer.append(dbpharm.get(i).getMedDispensed5());
-					writer.append('\n');
-			   }
-			   
-			
-			    writer.flush();
-			    writer.close();
-			}
-			catch(IOException e)
-			{
-				e.printStackTrace();
-			}
-	   }
-	   
-	   public static void generateRxReport()
-	   {
-		   String filename = null;
-		   JFileChooser jFileChooser = new JFileChooser();
-		   jFileChooser.setSelectedFile(new File(suggestFileName() + "-RxReport.csv"));
-		   int returnValue = jFileChooser.showSaveDialog(null);
-		   if ( returnValue == jFileChooser.APPROVE_OPTION )
-		   {
-			   File selectedFile = jFileChooser.getSelectedFile();
-			   generateRxFile(selectedFile);
-		   }
-	   }
-	 
-	   private static void generateRxFile(File filehandle)
-	   {
-		   try
-		   {
-			   FileWriter writer = new FileWriter(filehandle);
+			writer.flush();
+			writer.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 
-			   //Get Data from raptor
-			   	Session userSession = HibernateUtil.getSessionFactory().openSession();
-				userSession.beginTransaction();
-			
-				
-				List<PharmacyEncounter> dbpharm = userSession.createQuery( "from PharmacyEncounter").list();
-				List<Patient> dbpatient = userSession.createQuery( "from Patient").list();
-				userSession.getTransaction().commit(); 
-				userSession.close();
-			   
-			   writer.append("Rx Report");
-			   writer.append('\n');
-			   
-			  
-			   
-			   writer.append("Med Dispensed1");
-			   writer.append(',');
-			   writer.append("Equal Dispensed1");
-			   writer.append(',');
-			   writer.append("Med Dispensed2");
-			   writer.append(',');
-			   writer.append("Equal Dispensed2");
-			   writer.append(',');
-			   writer.append("Med Dispensed3");
-			   writer.append(',');
-			   writer.append("Equal Dispensed3");
-			   writer.append(',');
-			   writer.append("Med Dispensed4");
-			   writer.append(',');
-			   writer.append("Equal Dispensed4");
-			   writer.append(',');
-			   writer.append("Med Dispensed5");
-			   writer.append(',');
-			   writer.append("Equal Dispensed5");
-		       writer.append('\n');
-		 
-		       
-					for (int i = 0; i < dbpharm.size(); i++){
-						writer.append(dbpharm.get(i).getMedDispensed1());
-						writer.append(',');
-						writer.append(String.valueOf(dbpharm.get(i).getEqualPrescribed1()));
-						writer.append(',');
-						writer.append(dbpharm.get(i).getMedDispensed2());
-						writer.append(',');
-						writer.append(String.valueOf(dbpharm.get(i).getEqualPrescribed2()));
-						writer.append(',');
-						writer.append(dbpharm.get(i).getMedDispensed3());
-						writer.append(',');
-						writer.append(String.valueOf(dbpharm.get(i).getEqualPrescribed3()));
-						writer.append(',');
-						writer.append(dbpharm.get(i).getMedDispensed4());
-						writer.append(',');
-						writer.append(String.valueOf(dbpharm.get(i).getEqualPrescribed4()));
-						writer.append(',');
-						writer.append(dbpharm.get(i).getMedDispensed5());
-						writer.append(',');
-						writer.append(String.valueOf(dbpharm.get(i).getEqualPrescribed5()));
-						writer.append('\n');
-					}
-			   //generate whatever data you want
-		 
-			   writer.flush();
-			   writer.close();
+	public static void generateRxReport() {
+		String filename = null;
+		JFileChooser jFileChooser = new JFileChooser();
+		jFileChooser.setSelectedFile(new File(suggestFileName()
+				+ "-RxReport.csv"));
+		int returnValue = jFileChooser.showSaveDialog(null);
+		if (returnValue == jFileChooser.APPROVE_OPTION) {
+			File selectedFile = jFileChooser.getSelectedFile();
+			generateRxFile(selectedFile);
+		}
+	}
+
+	private static void generateRxFile(File filehandle) {
+		try {
+			FileWriter writer = new FileWriter(filehandle);
+
+			// Get Data from raptor
+			Session userSession = HibernateUtil.getSessionFactory()
+					.openSession();
+			userSession.beginTransaction();
+
+			List<PharmacyEncounter> dbpharm = userSession.createQuery(
+					"from PharmacyEncounter").list();
+			List<Patient> dbpatient = userSession.createQuery("from Patient")
+					.list();
+			userSession.getTransaction().commit();
+			userSession.close();
+
+			writer.append("Rx Report");
+			writer.append('\n');
+
+			writer.append("Med Dispensed1");
+			writer.append(',');
+			writer.append("Equal Dispensed1");
+			writer.append(',');
+			writer.append("Med Dispensed2");
+			writer.append(',');
+			writer.append("Equal Dispensed2");
+			writer.append(',');
+			writer.append("Med Dispensed3");
+			writer.append(',');
+			writer.append("Equal Dispensed3");
+			writer.append(',');
+			writer.append("Med Dispensed4");
+			writer.append(',');
+			writer.append("Equal Dispensed4");
+			writer.append(',');
+			writer.append("Med Dispensed5");
+			writer.append(',');
+			writer.append("Equal Dispensed5");
+			writer.append('\n');
+
+			for (int i = 0; i < dbpharm.size(); i++) {
+				writer.append(dbpharm.get(i).getMedDispensed1());
+				writer.append(',');
+				writer.append(String.valueOf(dbpharm.get(i)
+						.getEqualPrescribed1()));
+				writer.append(',');
+				writer.append(dbpharm.get(i).getMedDispensed2());
+				writer.append(',');
+				writer.append(String.valueOf(dbpharm.get(i)
+						.getEqualPrescribed2()));
+				writer.append(',');
+				writer.append(dbpharm.get(i).getMedDispensed3());
+				writer.append(',');
+				writer.append(String.valueOf(dbpharm.get(i)
+						.getEqualPrescribed3()));
+				writer.append(',');
+				writer.append(dbpharm.get(i).getMedDispensed4());
+				writer.append(',');
+				writer.append(String.valueOf(dbpharm.get(i)
+						.getEqualPrescribed4()));
+				writer.append(',');
+				writer.append(dbpharm.get(i).getMedDispensed5());
+				writer.append(',');
+				writer.append(String.valueOf(dbpharm.get(i)
+						.getEqualPrescribed5()));
+				writer.append('\n');
 			}
-			catch(IOException e)
-			{
-				e.printStackTrace();
-			}
-	   }
-	   
-	   private static String suggestFileName() {
-	   		
-		   String sfilename = "";
-		   Calendar rightNow = Calendar.getInstance();
-		   		
-		   //get the date
-		   int temp_int = rightNow.get(Calendar.YEAR);
-		   sfilename = sfilename + temp_int;
-		   temp_int = rightNow.get(Calendar.MONTH);
-		   temp_int++;
-		   if ( temp_int < 10 )
-			   sfilename = sfilename + "0";
-		   sfilename = sfilename + temp_int;
-		   temp_int = rightNow.get(Calendar.DAY_OF_MONTH);
-		   if ( temp_int < 10 )
-			   sfilename = sfilename + "0";
-		   sfilename = sfilename + temp_int + "-";
-		   //Get the time
-		   temp_int = rightNow.get(Calendar.HOUR);
-		   if ( rightNow.get(Calendar.AM_PM) == 0 )
-		   {
-			   if ( temp_int < 10 )
-				   sfilename = sfilename + "0";
-		   }
-		   else
-			   temp_int = temp_int + 12;
-		   
-		   sfilename = sfilename + temp_int;
-		   temp_int = rightNow.get(Calendar.MINUTE);
-		   sfilename = sfilename + temp_int;
-		   temp_int = rightNow.get(Calendar.SECOND);
-		   if ( temp_int < 10 )
-			   sfilename = sfilename + "0" + temp_int;
-		   else
-			   sfilename = sfilename + temp_int;
-	
-		   return sfilename;
-	   }
-	   
+			// generate whatever data you want
+
+			writer.flush();
+			writer.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private static String suggestFileName() {
+
+		String sfilename = "";
+		Calendar rightNow = Calendar.getInstance();
+
+		// get the date
+		int temp_int = rightNow.get(Calendar.YEAR);
+		sfilename = sfilename + temp_int;
+		temp_int = rightNow.get(Calendar.MONTH);
+		temp_int++;
+		if (temp_int < 10)
+			sfilename = sfilename + "0";
+		sfilename = sfilename + temp_int;
+		temp_int = rightNow.get(Calendar.DAY_OF_MONTH);
+		if (temp_int < 10)
+			sfilename = sfilename + "0";
+		sfilename = sfilename + temp_int + "-";
+		// Get the time
+		temp_int = rightNow.get(Calendar.HOUR);
+		if (rightNow.get(Calendar.AM_PM) == 0) {
+			if (temp_int < 10)
+				sfilename = sfilename + "0";
+		} else
+			temp_int = temp_int + 12;
+
+		sfilename = sfilename + temp_int;
+		temp_int = rightNow.get(Calendar.MINUTE);
+		sfilename = sfilename + temp_int;
+		temp_int = rightNow.get(Calendar.SECOND);
+		if (temp_int < 10)
+			sfilename = sfilename + "0" + temp_int;
+		else
+			sfilename = sfilename + temp_int;
+
+		return sfilename;
+	}
 
 }
