1cbb6b807f7d3305fdd73279b4d8b5b9739e63ad
mferrans <Michael.Ferrans@gmail.com>
2012-12-04 19:57:32 -0500

    PharmEncounter and PharmEncounterService returned to working

--- RaptorMRS/src/edu/wayne/cs/raptor/PharmacyEncounterService.java
+++ RaptorMRS/src/edu/wayne/cs/raptor/PharmacyEncounterService.java
@@ -4,17 +4,85 @@ import java.util.Calendar;
 
 import org.hibernate.Session;
 
-	public class PharmacyEncounterService {
-	
+public class PharmacyEncounterService {
+
+	protected int encounterID;
+	protected String medDispensed1;
+	protected String medDispensed2;
+	protected String medDispensed3;
+	protected String medDispensed4;
+	protected String medDispensed5;
+	protected boolean equalPrescribed1 = true;
+	protected boolean equalPrescribed2 = true;
+	protected boolean equalPrescribed3 = true;
+	protected boolean equalPrescribed4 = true;
+	protected boolean equalPrescribed5 = true;
+
 	private LoginBean login;
 	private Calendar calendar = Calendar.getInstance();
-	
+
 	private Session pharmSession;
-	
+
 	private PharmacyEncounter pharmEncounter;
-	
+
 	public PharmacyEncounterService() {
 		pharmEncounter = new PharmacyEncounter();
+	}
+
+	public int getEncounterID() {
+		return encounterID;
+	}
+
+	public void setEncounterID(int encounterID) {
+		this.encounterID = encounterID;
+	}
+
+	public String getMedDispensed1() {
+		return medDispensed1;
+	}
+
+	public void setMedDispensed1(String medDispensed1) {
+		this.medDispensed1 = medDispensed1;
+	}
+
+	public String getMedDispensed2() {
+		return medDispensed2;
+	}
+
+	public void setMedDispensed2(String medDispensed2) {
+		this.medDispensed2 = medDispensed2;
+	}
+
+	public String getMedDispensed3() {
+		return medDispensed3;
+	}
+
+	public void setMedDispensed3(String medDispensed3) {
+		this.medDispensed3 = medDispensed3;
+	}
+
+	public boolean getEqualPrescribed1() {
+		return equalPrescribed1;
+	}
+
+	public void setEqualPrescribed1(boolean equalPrescribed1) {
+		this.equalPrescribed1 = equalPrescribed1;
+	}
+
+	public boolean getEqualPrescribed2() {
+		return equalPrescribed2;
+	}
+
+	public void setEqualPrescribed2(boolean equalPrescribed2) {
+		this.equalPrescribed2 = equalPrescribed2;
+	}
+
+	public boolean getEqualPrescribed3() {
+		return equalPrescribed3;
+	}
+
+	public void setEqualPrescribed3(boolean equalPrescribed3) {
+		this.equalPrescribed3 = equalPrescribed3;
 	}
 
 	public Session getPharmSession() {
@@ -32,7 +100,38 @@ import org.hibernate.Session;
 	public void setPharmEncounter(PharmacyEncounter pharmEncounter) {
 		this.pharmEncounter = pharmEncounter;
 	}
-	
+
+	public String getMedDispensed4() {
+		return medDispensed4;
+	}
+
+	public void setMedDispensed4(String medDispensed4) {
+		this.medDispensed4 = medDispensed4;
+	}
+
+	public String getMedDispensed5() {
+		return medDispensed5;
+	}
+
+	public void setMedDispensed5(String medDispensed5) {
+		this.medDispensed5 = medDispensed5;
+	}
+
+	public boolean getEqualPrescribed4() {
+		return equalPrescribed4;
+	}
+
+	public void setEqualPrescribed4(boolean equalPrescribed4) {
+		this.equalPrescribed4 = equalPrescribed4;
+	}
+
+	public boolean getEqualPrescribed5() {
+		return equalPrescribed5;
+	}
+
+	public void setEqualPrescribed5(boolean equalPrescribed5) {
+		this.equalPrescribed5 = equalPrescribed5;
+	}
 
 	public LoginBean getLogin() {
 		return login;
@@ -41,10 +140,10 @@ import org.hibernate.Session;
 	public void setLogin(LoginBean login) {
 		this.login = login;
 	}
-	
+
 	public String dataToDatabase() {	
 		passToPharmEncounter();
-		
+
 		pharmSession = HibernateUtil.getSessionFactory().openSession();
 		pharmSession.beginTransaction();
 		pharmSession.saveOrUpdate(pharmEncounter);
@@ -53,19 +152,56 @@ import org.hibernate.Session;
 		/* must have encounterID
 		 * add encounterID to database
 		 * add existing medDispensed variables and corresponding equalPrescribed variables*/
-		
+
 		resetFields();
-		
-		return "pharm";
+
+		return "Valid";
 	}
-	
+
 	public void passToPharmEncounter(){
+		pharmEncounter.setEncounterID(encounterID);
+		pharmEncounter.setMedDispensed1(medDispensed1);
+		pharmEncounter.setMedDispensed2(medDispensed2);
+		pharmEncounter.setMedDispensed3(medDispensed3);
+		pharmEncounter.setMedDispensed4(medDispensed4);
+		pharmEncounter.setMedDispensed5(medDispensed5);
+		pharmEncounter.setEqualPrescribed1(equalPrescribed1);
+		pharmEncounter.setEqualPrescribed2(equalPrescribed2);
+		pharmEncounter.setEqualPrescribed3(equalPrescribed3);
+		pharmEncounter.setEqualPrescribed4(equalPrescribed4);
+		pharmEncounter.setEqualPrescribed5(equalPrescribed5);
+
 		pharmEncounter.setCreatingUser(this.login.getSystemUser().getUsername());
 		pharmEncounter.setCreatedDate(calendar.getTime());
 	}
-	
+
 	public void resetFields(){
-		setPharmEncounter(new PharmacyEncounter());
+		setEncounterID(0);
+		setMedDispensed1(null);
+		setMedDispensed2(null);
+		setMedDispensed3(null);
+		setMedDispensed4(null);
+		setMedDispensed5(null);
+		setEqualPrescribed1(true);
+		setEqualPrescribed2(true);
+		setEqualPrescribed3(true);
+		setEqualPrescribed4(true);
+		setEqualPrescribed5(true);
+
+		pharmEncounter.setEncounterID(encounterID);
+		pharmEncounter.setMedDispensed1(medDispensed1);
+		pharmEncounter.setMedDispensed2(medDispensed2);
+		pharmEncounter.setMedDispensed3(medDispensed3);
+		pharmEncounter.setMedDispensed4(medDispensed4);
+		pharmEncounter.setMedDispensed5(medDispensed5);
+		pharmEncounter.setEqualPrescribed1(equalPrescribed1);
+		pharmEncounter.setEqualPrescribed2(equalPrescribed2);
+		pharmEncounter.setEqualPrescribed3(equalPrescribed3);
+		pharmEncounter.setEqualPrescribed4(equalPrescribed4);
+		pharmEncounter.setEqualPrescribed5(equalPrescribed5);
+
+		pharmEncounter.setCreatingUser(null);
+		pharmEncounter.setCreatedDate(null);
 	}
 
 }
