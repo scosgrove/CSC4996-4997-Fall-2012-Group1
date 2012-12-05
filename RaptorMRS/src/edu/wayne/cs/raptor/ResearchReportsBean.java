334bd4ba5ccf3d40651e7e6672760b10ffa08516
lrubel <lrubel01@gmail.com>
2012-12-04 17:52:55 -0800

    updated report. Exports full report from condition report.

--- RaptorMRS/src/edu/wayne/cs/raptor/ResearchReportsBean.java
+++ RaptorMRS/src/edu/wayne/cs/raptor/ResearchReportsBean.java
@@ -157,50 +157,126 @@ public class ResearchReportsBean {
 					.openSession();
 			userSession.beginTransaction();
 
-			List<Encounter> dbencounter = userSession.createQuery(
-					"from Encounter").list();
-			List<PharmacyEncounter> dbpharm = userSession.createQuery(
-					"from PharmacyEncounter").list();
+			List<Encounter> dbencounter = userSession.createQuery("from Encounter").list();
+			List<PharmacyEncounter> dbpharm = userSession.createQuery("from PharmacyEncounter").list();
+			List<Patient>dbPatient = userSession.createQuery("from Patient").list();
 			userSession.getTransaction().commit();
 			userSession.close();
 
 			writer.append("Conditon Report");
 			writer.append('\n');
 
-			writer.append("Sample#");
+			writer.append("PatientID");
 			writer.append(',');
-			writer.append("Condition1");
+			writer.append("Village of Residence");
 			writer.append(',');
-			writer.append("Condition2");
+			writer.append("Birth Date");
 			writer.append(',');
-			writer.append("Condition3");
+			writer.append("Gender");
 			writer.append(',');
-			writer.append("Condition4");
+			writer.append("Height");
 			writer.append(',');
-			writer.append("Condition5");
+			writer.append("Weight");
 			writer.append(',');
-			writer.append("Rx Prescribed1");
+			writer.append("BMI");
 			writer.append(',');
-			writer.append("Rx Prescribed2");
+			writer.append("Blood Pressure (mmHg)");
 			writer.append(',');
-			writer.append("Rx Prescribed3");
+			writer.append("Heart Rate (bpm)");
 			writer.append(',');
-			writer.append("Rx Prescribed4");
+			writer.append("Temperature (C)");
 			writer.append(',');
-			writer.append("Rx Prescribed5");
+			writer.append("Respirations (Breaths per Minute)");
 			writer.append(',');
-			/*
-			 * writer.append("Rx Given1"); writer.append('\n');
-			 * writer.append("Rx Given2"); writer.append(',');
-			 * writer.append("Rx Given3"); writer.append(',');
-			 * writer.append("Rx Given4"); writer.append(',');
-			 * writer.append("Rx Given5");
-			 */
+			writer.append("Oxygen (%)");
+			writer.append(',');
+			writer.append("Chief Complaint");
+			writer.append(',');
+			writer.append("Treatment Given");
+			writer.append(',');
+			writer.append("Med Dispensed1");
+			writer.append(',');
+			writer.append("Med Dispensed2");
+			writer.append(',');
+			writer.append("Med Dispensed3");
+			writer.append(',');
+			writer.append("Med Dispensed4");
+			writer.append(',');
+			writer.append("Med Dispensed5");
+			writer.append(',');
+			writer.append("Blood Drawn");
+			writer.append(',');
+			writer.append("Sample #");
+			writer.append(',');
+			writer.append("Other Condition1");
+			writer.append(',');
+			writer.append("Other Condition2");
+			writer.append(',');
+			writer.append("Other Condition3");
+			writer.append(',');
+			writer.append("Other Condition4");
+			writer.append(',');
+			writer.append("Other Condition5");
+			writer.append(',');
+			// add HPI
+			writer.append("Radiation");
+			writer.append(',');
+			writer.append("Quality");
+			writer.append(',');
+			writer.append("Provokes/Palliates");
+			writer.append(',');
+			writer.append("Time of Day");
+			writer.append(',');
+			writer.append("Other");
+			writer.append(',');
+			writer.append("Overall Impression");
+			writer.append(',');
+			writer.append("Keywords from Previous Encounter");
+			
 			writer.append('\n');
 
 			for (int i = 0; i < dbencounter.size(); i++) {
-				writer.append(String.valueOf(dbencounter.get(i)
-						.getEncounterID()));
+				writer.append(String.valueOf(dbencounter.get(i).getPatientID()));
+				writer.append(',');
+				writer.append(dbPatient.get(i).getResidence());
+				writer.append(',');
+				writer.append(dbPatient.get(i).getBirthDate());
+				writer.append(',');
+				writer.append(dbPatient.get(i).getGender());
+				writer.append(',');
+				writer.append("get Height isn't there");
+				writer.append(',');
+				writer.append("not there");
+				writer.append(',');
+				writer.append("not there");
+				writer.append(',');
+				writer.append("not there");
+				writer.append(',');
+				writer.append("not there");
+				writer.append(',');
+				writer.append("not there");
+				writer.append(',');
+				writer.append("not there");
+				writer.append(',');
+				writer.append("not there");
+				writer.append(',');
+				writer.append(dbencounter.get(i).getChiefComplaint());
+				writer.append(',');
+				writer.append("not there");
+				writer.append(',');
+				writer.append(dbencounter.get(i).getMedicationPrescribed1());
+				writer.append(',');
+				writer.append(dbencounter.get(i).getMedicationPrescribed2());
+				writer.append(',');
+				writer.append(dbencounter.get(i).getMedicationPrescribed3());
+				writer.append(',');
+				writer.append(dbencounter.get(i).getMedicationPrescribed4());
+				writer.append(',');
+				writer.append(dbencounter.get(i).getMedicationPrescribed5());
+				writer.append(',');
+				writer.append("not there");
+				writer.append(',');
+				writer.append("not there");
 				writer.append(',');
 				writer.append(dbencounter.get(i).getCondition1());
 				writer.append(',');
@@ -212,15 +288,18 @@ public class ResearchReportsBean {
 				writer.append(',');
 				writer.append(dbencounter.get(i).getCondition5());
 				writer.append(',');
-				writer.append(dbpharm.get(i).getMedDispensed1());
+				writer.append(dbencounter.get(i).getRadiation());
 				writer.append(',');
-				writer.append(dbpharm.get(i).getMedDispensed2());
+				writer.append(dbencounter.get(i).getQuality());
 				writer.append(',');
-				writer.append(dbpharm.get(i).getMedDispensed3());
+				writer.append(dbencounter.get(i).getProvokes());
 				writer.append(',');
-				writer.append(dbpharm.get(i).getMedDispensed4());
+				writer.append(dbencounter.get(i).getTimeOfDay());
 				writer.append(',');
-				writer.append(dbpharm.get(i).getMedDispensed5());
+				writer.append(dbencounter.get(i).getOther());
+				writer.append(',');
+				writer.append(dbencounter.get(i).getOverallImpression());
+				
 				writer.append('\n');
 			}
 
